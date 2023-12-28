8271e1fbcff0edc5b821ef2421de6042139437fe
abhilashmullakkara <abhilssh85@gmail.com>
2023-12-28 21:01:22 +0530

    added another button in vie diary

--- app/src/main/java/com/abhilash/livedata/ui/theme/userdatabase/ViewDiaryScreen.kt
+++ app/src/main/java/com/abhilash/livedata/ui/theme/userdatabase/ViewDiaryScreen.kt
@@ -1,22 +1,25 @@
 package com.abhilash.livedata.ui.theme.userdatabase
 
+import android.content.Context
+import android.widget.Toast
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
@@ -46,8 +49,11 @@ fun ViewDiaryScreen(navController: NavController) {
     var share by rememberSaveable { mutableStateOf(false) }
     var ascend by rememberSaveable { mutableStateOf(false) }
     var result by rememberSaveable { mutableStateOf("") }
+    var toast by rememberSaveable { mutableStateOf(false) }
+
     val coroutineScope = rememberCoroutineScope()
     val context= LocalContext.current
+    var isDetailed by rememberSaveable { mutableStateOf(true) }
     var employeeInfo by rememberSaveable {
         mutableStateOf<List<Employee>>(emptyList())
     }
@@ -75,6 +81,7 @@ fun ViewDiaryScreen(navController: NavController) {
                 OutlinedButton(
                     onClick = {
                         flag = !flag
+                        isDetailed=false
                         coroutineScope.launch {
                             val data = StringBuffer()
                             employeeInfo= emptyList()
@@ -89,24 +96,54 @@ fun ViewDiaryScreen(navController: NavController) {
                             for (employe in employeeInfo) {
 
                                 data.append("\n   " + employe.id + ")            " + employe.dutyNo + "          " + employe.performedOn + "          " + employe.dutyEarned)
-                                // data.append(" " + employe.wayBillNo + " " + employe.employeeName + " " + employe.collection)
+                                 data.append(" " + employe.wayBillNo + " " + employe.employeeName + " " + employe.collection)
                             }
 
                             result = data.toString()
-                        }
+                      }
                     },
-                    modifier = Modifier.padding(start = 85.dp),
+                    modifier = Modifier.padding(start = 25.dp),
                     colors = ButtonDefaults.buttonColors(Color(0xFF456890))
                 )
                 {
                     Text("View ", color = Color.White, fontSize = 16.sp)
-                }
 
+                }
+                OutlinedButton(
+                    onClick = {
+                       // share=!share
+
+                              isDetailed=true
+                        flag = !flag
+                        employeeInfo= emptyList()
+                        coroutineScope.launch {
+                            employeeInfo = if (ascend) {
+                                ascend = false
+                                employeeInfo + EmployeeDB.getInstance(context).getEmployeeDao()
+                                    .displaylast()
+                            } else {
+                                ascend = true
+                                employeeInfo + EmployeeDB.getInstance(context).getEmployeeDao()
+                                    .display()
+                            }
+                            if(!toast){
+                                toast=true
+                                Toast.makeText(context, "Rotate the screen for better experince",Toast.LENGTH_SHORT-1000).show()
+                            }
+
+                        }
+                    },
+                    modifier = Modifier.padding(start = 20.dp),
+                    colors = ButtonDefaults.buttonColors(Color(0xFF456890))
+                )
+                {
+                    Text("Detailed View ", color = Color.White, fontSize = 16.sp)
+                }
                     OutlinedButton(
                         onClick = {
                            share=!share
                         },
-                        modifier = Modifier.padding(start = 85.dp),
+                        modifier = Modifier.padding(start = 25.dp),
                         colors = ButtonDefaults.buttonColors(Color(0xFF456890))
                     )
                     {
@@ -122,20 +159,39 @@ fun ViewDiaryScreen(navController: NavController) {
             Divider(color=Color.White)
             Spacer(modifier = Modifier.height(2.dp))
             Divider(color=Color.Green)
-            EmployeeList(employees = employeeInfo)
-            if(share){
-                SendWhatsAppMessage(context,result)
-            }
+            EmployeeList(employees = employeeInfo,context,result,share,isDetailed )
+//            if(share){
+//                SendWhatsAppMessage(context,result)
+//            }
         }
     }
 }
 @Composable
-fun EmployeeList(employees: List<Employee>) {
-    LazyColumn {
-        items(employees) { employee ->
-            EmployeeItem(employee = employee)
-            Divider(color = Color.Yellow)
+fun EmployeeList(employees: List<Employee>,context:Context,result:String,share:Boolean,isDetailed:Boolean) {
+    LazyColumn  {
+        item{
+            if(share){
+                SendWhatsAppMessage(context,result)
+
+            }
+            else
+            {
+                Text("\n")
+            }
+
         }
+
+        items(employees) { employee ->
+            if(isDetailed){
+                EmployeeItem(employee = employee)
+                // Divider(color = Color.Yellow)
+            }
+            else {
+                EmployeeItemReduced(employee = employee)
+            }
+            
+           }
+
     }
 }
 
@@ -194,4 +250,40 @@ fun BlinkingText(text: String) {
         }
     }
 }
+
+
+@Composable
+fun EmployeeItemReduced(employee: Employee) {
+    var color= Color.White
+    var sign=""
+    if(employee.dutySurrendered){
+        color=Color.Red
+        sign="*"
+    }
+    Row {
+        Spacer(modifier = Modifier.width(25.dp))
+        Text(text = " ${employee.id} )$sign",color=color,fontSize = 16.sp)
+        Spacer(modifier = Modifier.width(55.dp))
+        Text(employee.dutyNo,color=color,fontSize = 16.sp )
+        Spacer(modifier = Modifier.width(75.dp))
+        Text(employee.performedOn,color=color,fontSize = 16.sp )
+        Spacer(modifier = Modifier.width(105.dp))
+        Text(employee.dutyEarned, color=color,fontSize = 16.sp )
+//        Spacer(modifier = Modifier.width(65.dp))
+//        Text( employee.wayBillNo,color=color,fontSize = 16.sp)
+//        Spacer(modifier = Modifier.width(35.dp))
+//        Text(employee.employeeName,color=color,fontSize = 16.sp )
+//        Spacer(modifier = Modifier.width(45.dp))
+//        Text("₹",color=Color.Red, fontSize = 19.sp)
+//        Text(text = employee.collection,color=color,fontSize = 16.sp)
+//        Spacer(modifier = Modifier.width(35.dp))
+        if(employee.dutySurrendered){
+            BlinkingText(text = "☑")
+        }
+    }
+
+
+
+}
+
 
